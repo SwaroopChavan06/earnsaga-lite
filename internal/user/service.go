433f4da59a3b938bdf505f1c7715b11030a0d64d
Swaroop <swaroopch06@gmail.com>
2026-07-10 05:20:01 +0530

    refactor: consolidate user DTO into service file
    Signed-off-by: Swaroop <swaroopch06@gmail.com>

--- internal/user/service.go
+++ internal/user/service.go
@@ -4,6 +4,14 @@ import (
 	"context"
 )
 
+type UserProfileResponse struct {
+	ID        string `json:"id"`
+	Email     string `json:"email"`
+	Name      string `json:"name"`
+	AvatarURL string `json:"avatar_url"`
+	IsAdmin   bool   `json:"is_admin"`
+}
+
 type Service struct {
 	Repo *Repository
 }
